d438c8e0768324b351fa0a712ed04ea0589807e8
brian-peterson <bpete456@gmail.com>
2020-12-18 13:19:31 -0600

    added second piece of state to cleam up warning within search widget

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,46 +3,35 @@ import axios from 'axios';
 
 const Search = () => {
     const [term, setTerm] = useState('programming');
+    const [debouncedTerm, setDebouncedTerm] = useState(term);
     const [results, setResults ] = useState([]);
 
-    console.log(results);
+   useEffect(() => {
+       const timerId = setTimeout(() => {
+           setDebouncedTerm(term);
+       }, 1000);
 
-    useEffect(() => {
-        const search = async () => {
-            const {data} = await axios.get('https://en.wikipedia.org/w/api.php', {
-                params: {
-                    action: 'query',
-                    list: 'search',
-                    origin: '*',
-                    format: 'json',
-                    srsearch: term,
-                }
-            });
-            setResults(data.query.search);
-        };
+       return () => {
+           clearTimeout(timerId);
+       }
+   }, [term]);
 
-        if(term && !results.length) {
-            search();
-        } else {
- 
-            const timeoutId = setTimeout(() => {
-                if(term) search();
-            }, 500);
-    
-            return () => {
-                clearTimeout(timeoutId);
-            };           
-        }
+   useEffect(() => {
+    const search = async () => {
+        const {data} = await axios.get('https://en.wikipedia.org/w/api.php', {
+            params: {
+                action: 'query',
+                list: 'search',
+                origin: '*',
+                format: 'json',
+                srsearch: debouncedTerm,
+            },
+        });
+        setResults(data.query.search);
+    };
+        search();
+   }, [debouncedTerm])
 
-        const timeoutId = setTimeout(() => {
-            if(term) search();
-        }, 500);
-
-        return () => {
-            clearTimeout(timeoutId);
-        };
-    }, [term]);
-    
 
     const renderedResults = results.map((result) => {
         return (
